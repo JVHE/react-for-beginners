688572eba371b032520b29d769498e5fac5501a3
jvhe <jeongbae1212@gmail.com>
2022-09-21 22:38:32 +0900

    feat: useEffect를 이용하여 한 번만 실행하는 기능 추가 #6.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const onClick = () => setCount((c) => c + 1);
+  console.log('render always');
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (<div>
-    <h1 className={styles.title}>Hello, world!</h1>
-    <Button text={"hello"}/>
+    <h1>Count: {count}</h1>
+    <button onClick={onClick}>Increment</button>
   </div>
   );
 }
+
 
 export default App;
