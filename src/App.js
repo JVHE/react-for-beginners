4dca7d6354d6f2b606aa8865658330a38c38d9c7
jvhe <jeongbae1212@gmail.com>
2022-09-21 23:18:06 +0900

    feat: search bar 구현 및 특정 상황에서만 상태 변경하는 로직 구현 #6.2

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setCount((c) => c + 1);
-  console.log('render always');
+  const onChange = (e) => setKeyword(e.target.value);
   useEffect(() => {
-    console.log("CALL THE API");
+    console.log("I run only once");
   }, []);
-  return (<div>
-    <h1>Count: {count}</h1>
-    <button onClick={onClick}>Increment</button>
-  </div>
+  useEffect(() => {
+    console.log("I run only when keyword changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run only when count changes");
+  }, [count]);
+  useEffect(() => {
+    console.log("I run only when keyword or count changes");
+  }, [keyword, count]);
+  return (
+    <div>
+      <input type="text" placeholder='Search here...' onChange={onChange} />
+      <h1>Count: {count}</h1>
+      <button onClick={onClick}>Increment</button>
+    </div>
   );
 }
-
 
 export default App;
