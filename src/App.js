3414e93bc0823751ad3dc6d4c31b2fb311f0e92f
jvhe <jeongbae1212@gmail.com>
2022-09-22 16:22:55 +0900

    modify: todolist 제거 #7.2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import { useState } from 'react';
 
 function App() {
-  const [toDo, setToDo] = useState("")
-  const [toDoList, setToDoList] = useState([])
-  const onChange = (e) => {
-    setToDo(e.target.value)
-  }
-  const onSubmit = (e) => {
-    e.preventDefault()
-    if (toDo === "") {
-      return
-    }
-    setToDoList((currentArray) => [toDo, ...currentArray])
-    setToDo("")
-  }
-  console.log(toDoList);
   return (
     <div>
-      <h1>ToDo List ({toDoList.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your todo" />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDoList.map((item, index) => <li key={index}>{item}</li>)}
     </div>
   );
 }
