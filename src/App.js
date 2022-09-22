c51e9b2b41955629476a164b4168918f2fe85709
jvhe <jeongbae1212@gmail.com>
2022-09-22 16:43:55 +0900

    feat: simple coin tracker 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+    .then(response => response.json())
+    .then(json => {
+      setCoins(json);
+      setLoading(false);
+      console.log(json);
+    })
+  }, []);
+
   return (
     <div>
+      <h1>The Coins!</h1>
+      {loading ? <div>Loading...</div> : <div>{coins.length} coins loaded!</div>}
+      <ul>
+        {coins.map(coin => (
+          <li key={coin.id}>
+            <h2>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</h2>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
