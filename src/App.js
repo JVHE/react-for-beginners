eba40adfb75438cfca29303847f7517846a6d553
jvhe <jeongbae1212@gmail.com>
2022-09-22 18:03:57 +0900

    modify: 영화 앱을 만들기 위해 코드 정리 #7.3

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
 import { useState, useEffect } from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selectedCoin, setSelectedCoin] = useState("");
-  const [amount, setAmount] = useState(0);
-  useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then(response => response.json())
-      .then(json => {
-        setCoins(json);
-        if (json.length > 0) {
-          setSelectedCoin(json[0]);
-        }
-        setLoading(false);
-      })
-  }, []);
-
-  const handleSelect = (e) => {
-    const jsonObj = JSON.parse(e.target.value);
-    console.log(e.target.value);
-    setSelectedCoin(jsonObj);
-  }
-
   return (
     <div>
-      <h1>The Coins!</h1>
-      {loading ? <div>Loading...</div> :
-        (
-          <div>
-            <input type="number" onChange={e => setAmount(e.target.value)}
-              value={amount}
-              placeholder="Enter the USD" />
-
-            <select onChange={handleSelect}>
-              {coins.map((coin) => (
-                <option key={coin.id} value={JSON.stringify(coin)}>
-                  {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-                </option>
-              ))}
-            </select>
-            {selectedCoin && <h3>You can buy {amount / selectedCoin.quotes.USD.price} {selectedCoin.name}</h3>}
-          </div>
-        )}
     </div>
   );
 }
